169545d5e5d16a4fee283f70bc5315321dfa1299
Gabriel Souza <eugabrielf@gmail.com>
2023-01-25 15:52:58 -0300

    finalizando frontend

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,4 +1,4 @@
-import { Nav, Logo, H3, H4, Button, Span, Img, Cart } from "./styles";
+import { Nav, Logo, H3, H4, Button, Span, Img } from "./styles";
 import cart from "../../assets/icons/cart.png";
 
 export default function Navbar() {
@@ -13,25 +13,6 @@ export default function Navbar() {
         <Img src={cart} alt={cart} />
         <Span>0</Span>
       </Button>
-
-    {/*   <Cart>
-        <div className="item">1</div>
-        <div className="item">2</div>
-        <div className="item">3</div>
-        <div className="item">4</div>
-        <div className="item">5</div>
-        <div className="item">6</div>
-        <div className="item">7</div>
-        <div className="item">8</div>
-        <div className="item">9</div>
-        <div className="item">10</div>
-        <div className="item">11</div>
-        <div className="item">12</div>
-        <div className="item">13</div>
-        <div className="item">14</div>
-        <div className="item">15</div>
-        <div className="item">16</div>
-      </Cart> */}
     </Nav>
   );
 }
