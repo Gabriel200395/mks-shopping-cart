8500cec90ef7c73c22c6bd571b7ae9c06e3c2817
Gabriel Souza <eugabrielf@gmail.com>
2023-01-31 14:34:03 -0300

    organizando codigo

--- src/__test__/app.test.tsx
+++ src/__test__/app.test.tsx
@@ -33,11 +33,6 @@ describe("Testing Application", () => {
       "https://mks-challenge-api-frontend.herokuapp.com/api/v1/products?page=1&rows=8&sortBy=id&orderBy=ASC"
     );  
     expect(mockedAxios.get).toHaveBeenCalledTimes(1)
-
-
-  
-
-  
   });
 
   test("Testing cart without products", async () => {
